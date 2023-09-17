34aec426550acae314e4f8957742baa8cd0a977a
pajmd <philippejmd@gmail.com>
2023-09-17 16:57:55 +0200

    added uptime

--- root/netvue_report.sh
+++ root/netvue_report.sh
@@ -5,6 +5,15 @@
 #
 df -h | grep mmcblk0 > /dev/null
 if [ $? = 1 ]; then
-msg=$(date)" not mounted"
-echo $msg | nc 192.168.1.91 8888
+# msg=$(date)" mmcblk0 not mounted"
+msg="mmcblk0 not mounted"
+# echo $msg | nc 192.168.1.91 8888
+else
+# msg=$(date)" mmcblk0 still mounted"
+msg="mmcblk0 still mounted"
+# echo $msg | nc 192.168.1.91 8888
 fi
+upt=$(uptime)
+mnt=$(df | grep mmcblk0 | grep -v grep)
+echo $msg" - "$mnt" - "$upt | nc 192.168.1.91 8888
+
