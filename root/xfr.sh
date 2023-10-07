b7a8e15bfdc85b4d15c7576a3f7908ef3c0a96a6
pajmd <philippejmd@gmail.com>
2023-10-07 16:06:23 +0200

    Update xfr.sh

--- root/xfr.sh
+++ root/xfr.sh
@@ -1,19 +1,23 @@
 #/bin/sh
 
 # runs every 30sec
+last_xfer_log=last_xfer.log
 
 last_xfer=""
-if [ -f last_xfer.log]; then
-  last_xfer=$(cat last_xfer.log)
+if [ -e "${last_xfer_log}" ]; then
+  last_xfer=$(cat ${last_xfer_log})
 fi
   
 # Get the latest recording from the latest folder
 cd /mnt/TF/nvt3
 latest_folder=$(ls -dtrc1 */ | tail -1)
-ls -trc1 $latest_folder | tail -1
+latest_recording=$(ls -trc1 $latest_folder | tail -1)
 
-# TODO how do I transfer the folder/file name
-if [ "${lastet_recording} != "${last_xfer}"]; then
-  dd bs=16M if=${latest_folder}/${lastet_recording}  | nc 192.168.1.91 8556
+if [ "${latest_recording}" != "${last_xfer}" ]; then
+  # echo "${latest_folder}${lastet_recording}" > recording_name.log
+  tar cvf payload "${latest_folder}${latest_recording}"
+  dd bs=16M if=payload  | nc 192.168.1.91 8556
+  echo "${latest_recording}" > ${last_xfer_log}
 fi
+
 
