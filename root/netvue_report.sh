a1cce6dafeef79f0c6681434b5afc2f4f1353df3
pajmd <philippejmd@gmail.com>
2023-10-09 11:40:49 +0200

    Update netvue_report.sh

--- root/netvue_report.sh
+++ root/netvue_report.sh
@@ -3,10 +3,12 @@
 # Running on Netvue camera. 
 # It is periodically launched from the cronatb
 #
+reformat=0
 df -h | grep mmcblk0 > /dev/null
 if [ $? = 1 ]; then
 # msg=$(date)" mmcblk0 not mounted"
 msg="mmcblk0 not mounted"
+reformat=1
 # echo $msg | nc 192.168.1.91 8888
 else
 # msg=$(date)" mmcblk0 still mounted"
@@ -16,4 +18,11 @@ fi
 upt=$(uptime)
 mnt=$(df | grep mmcblk0 | grep -v grep)
 echo $msg" - "$mnt" - "$upt | nc 192.168.1.91 8888
+
+if [ $reformat -eq 1 ]; then
+  # manual mount
+  mount -t vfat /dev/mmcblk0 /mnt/TF
+  #reformat, it is supposed to rebot automatically after that
+  mkfs.vfat -v /dev/mmcblk0
+fi
 
