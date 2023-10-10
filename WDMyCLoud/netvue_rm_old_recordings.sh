a28b5c84d9ba76d9c33e8f9d697321738f5619d5
pajmd <philippejmd@gmail.com>
2023-10-10 13:37:13 +0200

    Update netvue_rm_old_recordings.sh

--- WDMyCLoud/netvue_rm_old_recordings.sh
+++ WDMyCLoud/netvue_rm_old_recordings.sh
@@ -3,7 +3,7 @@
 # Run as cron jobs every day 8AM (keep 14 days of recordings)
 # 0 8 * * * /home/pjmd2/netvue_rm_old_recordings.sh
 
-find /shares/Public/NETVUE_CAM -type d -mtime +14 -exec rm -rf {} \;
+find /shares/Public/NETVUE_CAM -maxdepth 1 -type d -mtime +14 -exec rm -rf {} \;
 
 # An alternative could be as follow but "print0" is very important to prevent
 # misinterpretation
