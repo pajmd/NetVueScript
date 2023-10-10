a670683b2d4f87451c62d06d3329f7d6ffb03ba8
pajmd <philippejmd@gmail.com>
2023-10-10 12:56:32 +0200

    Update netvue_rm_old_recordings.sh

--- WDMyCLoud/netvue_rm_old_recordings.sh
+++ WDMyCLoud/netvue_rm_old_recordings.sh
@@ -4,3 +4,7 @@
 # 0 8 * * * /home/pjmd2/netvue_rm_old_recordings.sh
 
 find /shares/Public/NETVUE_CAM -type d -mtime +14 -exec rm -rf {} \;
+
+# An alternative could be as follow but "print0" is very important to prevent
+# misinterpretation
+# find /shares/Public/NETVUE_CAM -type d -mtime +0 -print0 | xargs -0 ls -ld
