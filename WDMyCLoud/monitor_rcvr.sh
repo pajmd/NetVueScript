c651efa798c931da1189ee9cd963d74d5769eab7
pajmd <philippejmd@gmail.com>
2023-12-11 11:01:10 +0100

    Update monitor_rcvr.sh

--- WDMyCLoud/monitor_rcvr.sh
+++ WDMyCLoud/monitor_rcvr.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 now=$(date +'%s')
-for pt in $(ps -ef | grep "dd bs" | grep -v grep | awk '{print $2";"$5}');
+for pt in $(ps -ef | grep "dd bs" | grep -v grep | awk '{print $2";"$3";"$5}');
 do
-  t=$(echo $pt | cut -d";" -f2)
+  t=$(echo $pt | cut -d";" -f3)
   pid=$(echo $pt | cut -d";" -f1)
+  ppid=$(echo $pt | cut -d";" -f2)
   start_ts=$(date +'%s' -d$t)
   let run_ts=$now-$start_ts
   echo "runtime: ${run_ts}"
   if [ $run_ts -ge 300 ]; then
-    echo "bounce rcvr ${pid}"
+    echo "bounce rcvr ${ppid}"
   fi
 done
