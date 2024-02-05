37495e089bbdb9e94d1cbda435a89460dee1deab
pajmd <philippejmd@gmail.com>
2024-02-05 18:34:20 +0100

    Update monitor_rcvr.sh

--- WDMyCLoud/monitor_rcvr.sh
+++ WDMyCLoud/monitor_rcvr.sh
@@ -5,7 +5,7 @@ ports=("8556" "8557")
 
 for port in ${ports[@]};
 do
-   echo "Checking on $(date) port $port"
+   echo "Checking on $(date) port $port" >> /home/pjmd2/bounced.log
    pids=$(netstat -atp | grep $port | grep -v grep | grep ESTABLISHED | awk '{print $ 7}' | awk -F/ '{print $1}')
    echo "Pids: $pids"
    if  [ "$pids" != "" ]; then
