013b5bef935369cbcbe5ca83a01f89fec4e4ab65
pajmd <philippejmd@gmail.com>
2024-06-21 15:04:08 +0200

    Update monitor_rcvr.sh

--- OMV/monitor_rcvr.sh
+++ OMV/monitor_rcvr.sh
@@ -7,17 +7,17 @@ for port in ${ports[@]};
 do
    echo "Checking on $(date) port $port" >> /home/pjmd/netvue/logs/bounced.log
    pids=$(netstat -atp | grep $port | grep -v grep | grep ESTABLISHED | awk '{print $ 7}' | awk -F/ '{print $1}')
-   echo "Pids: $pids"
+   # echo "Pids: $pids"
    if  [ "$pids" != "" ]; then
       for pid in $pids;
       do
          pt=$(ps -ef | grep "nc -l -p $port" | grep $pid | grep -v grep | awk '{print $2";"$3";"$5";"$11}')
-         echo $pt
+         # echo $pt
          t=$(echo $pt | cut -d";" -f3)
          ppid=$(echo $pt | cut -d";" -f2)
          start_ts=$(date +'%s' -d$t)
          let run_ts=$now-$start_ts
-         echo "runtime: ${run_ts}"
+         # echo "runtime: ${run_ts}"
          if [ $run_ts -ge 300 ]; then
             echo "$(date) - now ${now} pid: ${pid} ppid: ${ppid} port: ${port} is stuck since ${start_ts} bounce it" >> /home/pjmd/netvue/logs/bounced.log
             if [ $ppid -eq 1 ]; then
@@ -30,7 +30,7 @@ do
               fi
               echo "Automatically Running: service ${service} restart" >> /home/pjmd/netvue/logs/bounced.log
               kill -9 $ppid
-#              sleep 2
+              sleep 2
 #              systemctl restart $service
             fi
          fi
