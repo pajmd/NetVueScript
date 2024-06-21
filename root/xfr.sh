ef0a6c838bf6cd1957ffe97e3f0366fc23a74dd2
pajmd <philippejmd@gmail.com>
2024-06-21 19:23:30 +0200

    sending to OMV. Figur out next free port

--- root/xfr.sh
+++ root/xfr.sh
@@ -9,12 +9,30 @@ if [ -e "${last_xfer_log}" ]; then
   last_xfer=$(cat ${last_xfer_log})
 fi
 
-port_num=8556
-if [ -e "${port_log}" ]; then
-  port_num=$(cat ${port_log})
+# port_num=8556
+# if [ -e "${port_log}" ]; then
+#   port_num=$(cat ${port_log})
+# fi
+# echo $((port_num == 8556 ? 8557 : 8556)) > ${port_log}
+
+# what port is free
+port=$(ps -ef | grep "nc " | grep 8557)
+if [ -z "$port" ]
+then
+   port_num=8557
+else 
+   port=$(ps -ef | grep "nc " | grep 8556)
+   if [ -z "$port" ]
+   then
+      port_num=8556
+   else
+      # Just wait until a port is free
+      echo "$(date) Both ports are busy" > ${port_log}
+      exit 0
+   fi
 fi
-echo $((port_num == 8556 ? 8557 : 8556)) > ${port_log}
-  
+echo "$(date) $port_num" > ${port_log}
+
 # Get the latest recording from the latest folder
 cd /mnt/TF/nvt3
 latest_folder=$(ls -dtrc1 */ | tail -1)
@@ -24,7 +42,7 @@ if [ "${latest_recording}" != "${last_xfer}" ]; then
   echo "${latest_recording}" > ${last_xfer_log}
   # echo "${latest_folder}${lastet_recording}" > recording_name.log
   tar cvf payload_$port_num "${latest_folder}${latest_recording}"
-  dd bs=16M if=payload_$port_num  | nc 192.168.1.91 $port_num
+  dd bs=16M if=payload_$port_num  | nc 192.168.1.33 $port_num
   rm payload_$port_num
 fi
 
